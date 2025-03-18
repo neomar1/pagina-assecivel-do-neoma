f32ade85bfdcd76c124efcce0c565da46f12786d
neomar1 <159184966+neomar1@users.noreply.github.com>
2025-03-18 11:19:18 -0300

    dia 18/03

---
+++ site-acessivel-projeto-inicial/sript.js
@@ -0,0 +1,9 @@
+document. addEventListener('DOMContentLoaded' , function ()) {
+     const aumentaFonteBotao= document.getElementById('aumenta-font');
+     const diminuiFonteBotao= document.getElementById('diminui-font');
+     
+
+     led tamanhAtualFont= 1;
+
+     aumentaFonteBotao. addEventListener ('click' ,
+}
